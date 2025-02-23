a3b29cc23e6f574f199f7058cd61d48810774d89
JeXps <juandavidespinelpena@gmail.com>
2025-02-23 09:38:32 -0500

    Corregir configuración de DATABASE_URL en PostgreSQL

--- config/database.php
+++ config/database.php
@@ -82,9 +82,17 @@ return [
             ]) : [],
         ],
 
+        <?php
+
+return [
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
+    'connections' => [
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -96,6 +104,12 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+
+    ],
+
+    'migrations' => 'migrations',
+
+];
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
