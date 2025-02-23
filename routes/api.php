e9e4a3d53b911a80230d08a30287dc6a949bbfdd
JeXps <juandavidespinelpena@gmail.com>
2025-02-23 13:24:49 -0500

    Corregir errores

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,19 @@ use App\Http\Controllers\UserController;
 Route::get('/test', function () {
     return response()->json(['message' => 'Esta API fue realizada por JUAN DAVID ESPINEL PEÑA']);
 });
+Route::get('/info', function () {
+    return response()->json([
+        'message' => 'API funcionando',
+        'endpoints' => [
+            'GET /api/categories',
+            'POST /api/categories',
+            'GET /api/categories/{id}',
+            'PUT /api/categories/{id}',
+            'DELETE /api/categories/{id}',
+        ]
+    ]);
+});
+
 
 // Rutas de API para products, categories y users
 Route::apiResource('products', ProductController::class);
